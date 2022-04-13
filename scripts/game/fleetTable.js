7f162ccb30c287a6d0a41fc0c365675b16367ec9
packet3 <joe.yobo@gmail.com>
2022-04-13 19:48:57 +0100

    rewrote fleettable js and fixed styling

--- scripts/game/fleetTable.js
+++ scripts/game/fleetTable.js
@@ -1,12 +1,40 @@
-$(function() {
-	window.setInterval(function() {
-		$('.fleets').each(function() {
-			var s		= $(this).data('fleet-time') - (serverTime.getTime() - startTime) / 1000;
-			if(s <= 0) {
-				$(this).text('-');
+/**
+ *   orginal code based on New Star game, 
+ *   re-wrote code to not use Jquery - Joe
+ *
+ *  XSpace (xspacewars.com)
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * @package XSpace
+ * @author joe <joe@xspacewars.com>
+ * @copyright 2022 Joe
+ * @licence MIT
+ * @version 1.0.0
+ * @link https://github.com/packet3/xspacewars
+ */
+
+$(document).ready(function () {
+	window.setInterval(function () {
+		let fleets = document.getElementsByClassName('fleets')
+		for (let i = 0; i < fleets.length; i++) {
+			let s = fleets[i].getAttribute('fleet-time')
+			s -= (serverTime.getTime() - startTime) / 1000
+
+			if (s <= 0) {
+				fleets[i].textContent = '-'
+
 			} else {
-				$(this).text(GetRestTimeFormat(s));
+				fleets[i].textContent = GetRestTimeFormat(s)
+
 			}
-		})
+		}
+		// $('.fleets').each(function() {
+		// 	var s		= $(this).data('fleet-time') - (serverTime.getTime() - startTime) / 1000;
+		// 	if(s <= 0) {
+		// 		$(this).text('-');
+		// 	} else {
+		// 		$(this).text(GetRestTimeFormat(s));
+		// 	}
+		// })
 	}, 1000);
 });

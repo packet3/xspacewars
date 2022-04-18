e35d378e8ae932261e74e94c617950ad5c3f6753
packet3 <joe.yobo@gmail.com>
2022-04-18 12:36:12 +0100

    rewrote officier js

--- scripts/game/officier.js
+++ scripts/game/officier.js
@@ -1,9 +1,23 @@
-function GetOfficerTime(Element, Time)
-{
-	if(Time == 0)
+/**
+ *   orginal code based on New Star game, 
+ *   re-wrote code to not use Jquery - Joe
+ *
+ *  XSpace (xspacewars.com)
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * @package XSpace
+ * @author joe <joe@xspacewars.com>
+ * @copyright 2022 Joe
+ * @licence MIT
+ * @version 1.0.0
+ * @link https://github.com/packet3/xspacewars
+ */
+function GetOfficerTime(Element, Time) {
+	if (Time == 0)
 		return;
-	
-	$('#time_'+Element).text(GetRestTimeFormat(Time));
+
+	document.querySelector('#time_' + Element).textContent = GetRestTimeFormat(Time)
+	// $('#time_'+Element).text(GetRestTimeFormat(Time));
 	Time--;
-	window.setTimeout("GetOfficerTime("+Element+", "+Time+")", 1000)
+	window.setTimeout("GetOfficerTime(" + Element + ", " + Time + ")", 1000)
 }

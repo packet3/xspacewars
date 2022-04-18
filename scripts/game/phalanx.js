d5db4a84b2231b6cec4c97fee131b12e7cbc9354
packet3 <joe.yobo@gmail.com>
2022-04-18 15:19:47 +0100

    rewrite phalanx

--- scripts/game/phalanx.js
+++ scripts/game/phalanx.js
@@ -1,15 +1,43 @@
-$(document).ready(function(){
+/**
+ *   orginal code based on New Star game, 
+ *   re-wrote code to not use Jquery - Joe
+ *
+ *  XSpace (xspacewars.com)
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * @package XSpace
+ * @author joe <joe@xspacewars.com>
+ * @copyright 2022 Joe
+ * @licence MIT
+ * @version 1.0.0
+ * @link https://github.com/packet3/xspacewars
+ */
+
+$(document).ready(function () {
 	FleetTime();
 });
 
 function FleetTime() {
-	$('.fleets').each(function() {
-		var s		= $(this).data('fleet-time') - (serverTime.getTime() - startTime) / 1000;
-		if(s <= 0) {
-			$(this).text('-');
+	let fleets = document.querySelectorAll('.fleets')
+	for (let i = 0; i < fleets.length; i++) {
+		let fleetTime = fleets[i].getAttribute('fleet-time')
+		let s = fleetTime - (serverTime.getTime() - startTime) / 1000
+
+		if (s <= 0) {
+			fleets[i].textContent = '-'
+
 		} else {
-			$(this).text(GetRestTimeFormat(s));
+			fleets[i].textContent = GetRestTimeFormat(s)
+
 		}
-	});
+	}
+	// $('.fleets').each(function() {
+	// 	var s		= $(this).data('fleet-time') - (serverTime.getTime() - startTime) / 1000;
+	// 	if(s <= 0) {
+	// 		$(this).text('-');
+	// 	} else {
+	// 		$(this).text(GetRestTimeFormat(s));
+	// 	}
+	// });
 	window.setTimeout('FleetTime()', 1000);
 }

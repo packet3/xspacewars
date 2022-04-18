4a01a4f8336a6771c2f2b06e38f7cea823639643
packet3 <joe.yobo@gmail.com>
2022-04-18 18:36:10 +0100

    rewrote price_al.js

--- scripts/game/price_al.js
+++ scripts/game/price_al.js
@@ -1,111 +1,146 @@
-function Price901(Element, Price)
-{
-	var Amount  	= parseInt($('#amount'+Element).val());
-	var metal 	    = parseInt($('#res_901').text().replace(/\./g, ''));
-	var TotalPrice	= Price * Amount;
-	
-	var PriceE		= $('#price'+Element);
-	
-	if(TotalPrice <= metal)
-		PriceE.css('color','lime');
-	else
-		PriceE.css('color','red');
+/**
+ *   orginal code based on New Star game, 
+ *   re-wrote code to not use Jquery - Joe
+ *
+ *  XSpace (xspacewars.com)
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * @package XSpace
+ * @author joe <joe@xspacewars.com>
+ * @copyright 2022 Joe
+ * @licence MIT
+ * @version 1.0.0
+ * @link https://github.com/packet3/xspacewars
+ */
 
-	PriceE.html(NumberGetHumanReadable(TotalPrice));
+function Price901(Element, Price) {
+	var Amount = parseInt(document.querySelector('#amount' + Element).value) //$('#amount'+Element).val());
+	var metal = parseInt(document.querySelector('#res_901').textContent.replace((/\./g, ''))); //$('#res_901').text().replace(/\./g, ''));
+	var TotalPrice = Price * Amount;
+
+	var PriceE = document.querySelector('#price' + Element) //$('#price'+Element);
+
+	if (TotalPrice <= metal)
+		PriceE.style.color = 'lime'
+	// PriceE.css('color','lime');
+	else
+		PriceE.style.color = 'red'
+	// PriceE.css('color','red');
+
+	PriceE.innerHTML = NumberGetHumanReadable(TotalPrice)
+	// PriceE.html(NumberGetHumanReadable(TotalPrice));
 }
 
-function Price902(Element, Price)
-{
-	var Amount  	= parseInt($('#amount'+Element).val());
-	var crystal 	= parseInt($('#res_902').text().replace(/\./g, ''));
-	var TotalPrice	= Price * Amount;
-	
-	var PriceE		= $('#price'+Element);
-	
-	if(TotalPrice <= crystal)
-		PriceE.css('color','lime');
-	else
-		PriceE.css('color','red');
+function Price902(Element, Price) {
+	var Amount = parseInt(document.querySelector('#amount' + Element).value) //$('#amount'+Element).val());
+	var crystal = parseInt(document.querySelector('#res_902').textContent.replace((/\./g, ''))); //$('#res_901').text().replace(/\./g, ''));
+	var TotalPrice = Price * Amount;
 
-	PriceE.html(NumberGetHumanReadable(TotalPrice));
+	var PriceE = document.querySelector('#price' + Element) //$('#price'+Element);
+
+	if (TotalPrice <= crystal)
+		PriceE.style.color = 'lime'
+	// PriceE.css('color','lime');
+	else
+		PriceE.style.color = 'red'
+	// PriceE.css('color','red');
+
+	PriceE.innerHTML = NumberGetHumanReadable(TotalPrice)
+
+
 }
 
-function Price903(Element, Price)
-{
-	var Amount  	= parseInt($('#amount'+Element).val());
-	var deuterium 	= parseInt($('#res_903').text().replace(/\./g, ''));
-	var TotalPrice	= Price * Amount;
-	
-	var PriceE		= $('#price'+Element);
-	
-	if(TotalPrice <= deuterium)
-		PriceE.css('color','lime');
-	else
-		PriceE.css('color','red');
+function Price903(Element, Price) {
+	var Amount = parseInt(document.querySelector('#amount' + Element).value) //$('#amount'+Element).val());
+	var deuterium = parseInt(document.querySelector('#res_903').textContent.replace((/\./g, ''))); //$('#res_901').text().replace(/\./g, ''));
+	var TotalPrice = Price * Amount;
 
-	PriceE.html(NumberGetHumanReadable(TotalPrice));
+	var PriceE = document.querySelector('#price' + Element) //$('#price'+Element);
+
+	if (TotalPrice <= deuterium)
+		PriceE.style.color = 'lime'
+	// PriceE.css('color','lime');
+	else
+		PriceE.style.color = 'red'
+	// PriceE.css('color','red');
+
+	PriceE.innerHTML = NumberGetHumanReadable(TotalPrice)
+
+
 }
 
-function Price921(Element, Price)
-{
-	var Amount  	= parseInt($('#amount'+Element).val());
-	var darkmatter 	= parseInt(($('#res_921').attr('name')).text().replace(/\./g, ''));
-	var TotalPrice	= Price * Amount;
-	
-	var PriceE		= $('#price'+Element);
-	
-	if(TotalPrice <= darkmatter)
-		PriceE.css('color','lime');
-	else
-		PriceE.css('color','red');
+function Price921(Element, Price) {
+	var Amount = parseInt(document.querySelector('#amount' + Element).value) //$('#amount'+Element).val());
+	var darkmatter = parseInt(document.querySelector('#res_921').textContent.replace((/\./g, ''))); //$('#res_901').text().replace(/\./g, ''));
+	var TotalPrice = Price * Amount;
 
-	PriceE.html(NumberGetHumanReadable(TotalPrice));
+	var PriceE = document.querySelector('#price' + Element) //$('#price'+Element);
+
+	if (TotalPrice <= darkmatter)
+		PriceE.style.color = 'lime'
+	// PriceE.css('color','lime');
+	else
+		PriceE.style.color = 'red'
+	// PriceE.css('color','red');
+
+	PriceE.innerHTML = NumberGetHumanReadable(TotalPrice)
+
+
 }
 
-function Price922(Element, Price)
-{
-	var Amount  	= parseInt($('#amount'+Element).val());
-	var antimatter 	= parseInt($('#res_922').text().replace(/\./g, ''));
-	var TotalPrice	= Price * Amount;
-	
-	var PriceE		= $('#price'+Element);
-	
-	if(TotalPrice <= antimatter)
-		PriceE.css('color','lime');
-	else
-		PriceE.css('color','red');
+function Price922(Element, Price) {
+	var Amount = parseInt(document.querySelector('#amount' + Element).value) //$('#amount'+Element).val());
+	var antimatter = parseInt(document.querySelector('#res_922').textContent.replace((/\./g, ''))); //$('#res_901').text().replace(/\./g, ''));
+	var TotalPrice = Price * Amount;
 
-	PriceE.html(NumberGetHumanReadable(TotalPrice));
+	var PriceE = document.querySelector('#price' + Element) //$('#price'+Element);
+
+	if (TotalPrice <= antimatter)
+		PriceE.style.color = 'lime'
+	// PriceE.css('color','lime');
+	else
+		PriceE.style.color = 'red'
+	// PriceE.css('color','red');
+
+	PriceE.innerHTML = NumberGetHumanReadable(TotalPrice)
+
+
 }
 
-function Price923(Element, Price)
-{
-	var Amount  	= parseInt($('#amount'+Element).val());
-	var stardust 	= parseInt($('#res_923').text().replace(/\./g, ''));
-	var TotalPrice	= Price * Amount;
-	
-	var PriceE		= $('#price'+Element);
-	
-	if(TotalPrice <= stardust)
-		PriceE.css('color','lime');
-	else
-		PriceE.css('color','red');
+function Price923(Element, Price) {
+	var Amount = parseInt(document.querySelector('#amount' + Element).value) //$('#amount'+Element).val());
+	var stardust = parseInt(document.querySelector('#res_923').textContent.replace((/\./g, ''))); //$('#res_901').text().replace(/\./g, ''));
+	var TotalPrice = Price * Amount;
 
-	PriceE.html(NumberGetHumanReadable(TotalPrice));
+	var PriceE = document.querySelector('#price' + Element) //$('#price'+Element);
+
+	if (TotalPrice <= stardust)
+		PriceE.style.color = 'lime'
+	// PriceE.css('color','lime');
+	else
+		PriceE.style.color = 'red'
+	// PriceE.css('color','red');
+
+	PriceE.innerHTML = NumberGetHumanReadable(TotalPrice)
+
+
 }
 
-function Price924(Element, Price)
-{
-	var Amount  	= parseInt($('#amount'+Element).val());
-	var contein 	= parseInt($('#res_924').text().replace(/\./g, ''));
-	var TotalPrice	= Price * Amount;
-	
-	var PriceE		= $('#price'+Element);
-	
-	if(TotalPrice <= contein)
-		PriceE.css('color','lime');
-	else
-		PriceE.css('color','red');
+function Price924(Element, Price) {
+	var Amount = parseInt(document.querySelector('#amount' + Element).value) //$('#amount'+Element).val());
+	var contein = parseInt(document.querySelector('#res_924').textContent.replace((/\./g, ''))); //$('#res_901').text().replace(/\./g, ''));
+	var TotalPrice = Price * Amount;
 
-	PriceE.html(NumberGetHumanReadable(TotalPrice));
+	var PriceE = document.querySelector('#price' + Element) //$('#price'+Element);
+
+	if (TotalPrice <= contein)
+		PriceE.style.color = 'lime'
+	// PriceE.css('color','lime');
+	else
+		PriceE.style.color = 'red'
+	// PriceE.css('color','red');
+
+	PriceE.innerHTML = NumberGetHumanReadable(TotalPrice)
+
+
 }

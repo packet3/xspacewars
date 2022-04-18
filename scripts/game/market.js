d4769bb2a3a6df97a3a2ea068c0387c2f32a0b61
packet3 <joe.yobo@gmail.com>
2022-04-18 12:03:27 +0100

    rewrote market.js

--- scripts/game/market.js
+++ scripts/game/market.js
@@ -1,13 +1,41 @@
-function open_market(IDblock, IDopen)
-{
-	if(!$('.gr_btn_top a.'+ IDblock).hasClass('active'))
-	{
-        $('.gr_btn_top a').removeClass('active');	
-        $('.gr_btn_top a.'+ IDblock).addClass('active');
-		$('.markett_conteiner').hide();
-		$('#' + IDblock).show();
-		
-		if(IDopen != 'no')
-			$.cookie("open_market", IDopen);	
+/**
+ *   orginal code based on New Star game, 
+ *   re-wrote code to not use Jquery - Joe
+ *
+ *  XSpace (xspacewars.com)
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * @package XSpace
+ * @author joe <joe@xspacewars.com>
+ * @copyright 2022 Joe
+ * @licence MIT
+ * @version 1.0.0
+ * @link https://github.com/packet3/xspacewars
+ */
+function open_market(IDblock, IDopen) {
+
+	let btnTopaID = document.querySelectorAll('.gr_btn_top a.' + IDblock)
+	let btnTopa = document.querySelectorAll('.gr_btn_top a')
+	let markettContainer = document.querySelectorAll('.markett_conteiner')
+
+	if (!btnTopaID[0].classList.contains('active')) {
+		for (let i = 0; i < btnTopa.length; i++) {
+			btnTopa[i].classList.remove('active')
+		}
+		btnTopaID[0].classList.add('active')
+
+		for (let i = 0; i < markettContainer.length; i++) {
+			markettContainer[i].style.display = 'none'
+		}
+
+		document.querySelector('#' + IDblock).style.display = 'block'
+
+		if (IDopen != 'no')
+			document.cookie = "open_market=" + IDopen
+
+
+
+
 	}
+
 }
